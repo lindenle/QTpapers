c9c6587be8748b129efb520013690bb0f9deef25
Loren A. Linden Levy <lindenle@uiuc.edu>
2011-04-13 16:32:32 -0700

    working on the rss feed window

--- RssWidget.cpp
+++ RssWidget.cpp
@@ -2,13 +2,40 @@
 #include "RssWidget.h"
 
 RssWidget::RssWidget() :
-  _stdout(stdout)
+  _stdout(stdout),
+  _current_rss("http://arxiv.org/")
 {
-  
-  load(QUrl("http://arxiv.org/"));
+  //first we have to pack the two new widgets up;
 
-  show();
+  _rss_view = new QWebView();
+  _rss_list = new QComboBox();
+
+  _rss_list->addItem(tr("Arxiv"));
+  _rss_list->addItem(tr("New RSS"));
+
+  connect(_rss_list, SIGNAL(currentIndexChanged), this, SLOT());
+
+  addWidget(_rss_list);  
+  addWidget(_rss_view);  
+
+  setOrientation(Qt::Vertical);
+
+  //here we need to make it so that the url drop down list can be
+  //selected and then it will change the rss view
+  update_web_view();
 }
 
+void RssWidget::set_current_rss()
+{
+  _stdout << "set_current_rss"  << endl;
+  //  _current_rss = _rss_list->...
+  update_web_view();
+}
 
+void RssWidget::update_web_view()
+{
+  _rss_view->load(QUrl(_current_rss));
+
+  _rss_view->show();
+}
 
--- RssWidget.h
+++ RssWidget.h
@@ -2,16 +2,22 @@
 #define __RSS_WIDGET__
 
 #include <QWebView>
+#include <QComboBox>
+#include <QSplitter>
 #include <QStringList>
 #include <QTextStream>
 
-class RssWidget : public QWebView
+class RssWidget : public QSplitter
 {
   Q_OBJECT
 
  public:
 
   RssWidget();
+
+  QUrl url(){return _rss_view->url();}
+
+  QWebPage * page(){return _rss_view->page();}
   
  private slots:
 
@@ -19,16 +25,24 @@ class RssWidget : public QWebView
 
   void delete_feed(){;}
 
+  void update_web_view();
+
+  void set_current_rss();
+
  private:
 
+  QAction * addFeed;    
+  QAction * deleteFeed;    
 
  protected:
   
   //Need a list of all the rss feeds that we have
-  QStringList _feeds;
-  QString _current_feed;
   QTextStream _stdout;
-
+  QString _current_rss;
+  QStringList _feeds;
+  QWebView *  _rss_view;
+  QComboBox * _rss_list;
+  
 };
 
 #endif //__RSS_WIDGET__
